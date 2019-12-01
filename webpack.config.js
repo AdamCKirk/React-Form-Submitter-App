ac753cda16a9a9a059e03a6b2a4e02789b31eeae
Adam <adam_kirk_msc@hotmail.com>
2019-12-01 20:41:14 +0000

    Installed express and setup localhost:3000
    I have installed the express package using yarn and I have setup a basic system taht gets the html and other static files.

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,31 @@
 const path = require('path');
 
-module.exports = {
-    entry: './src/app.js',
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
-    module : {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.js$/,
-            exclude: /node_modules/
-        }, {
-            use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader'
-            ],
-            test: /\.sass$/
-        }]
-    },
-    devtool: 'cheap-module-eval-source-map',
-    devServer: {
-        contentBase: path.join(__dirname, 'public')
-    }
-};
+module.exports = (env) => {
+    const isProduction = env === 'production';
+
+    return {
+        entry: './src/app.js',
+        output: {
+            path: path.join(__dirname, 'public'),
+            filename: 'bundle.js'
+        },
+        module : {
+            rules: [{
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/
+            }, {
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ],
+                test: /\.sass$/
+            }]
+        },
+        devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
+        devServer: {
+            contentBase: path.join(__dirname, 'public')
+        }
+    };
+}
