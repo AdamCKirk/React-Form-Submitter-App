be66e25a47903197807b040b9617740cef83a9b4
Adam <adam_kirk_msc@hotmail.com>
2019-12-02 09:53:22 +0000

    Responce Log and Syntax Update
    I have removed the responce log details and I have also updated the server running code to be more clear. I have also updated my syntax as I was missing a few semicolons at the end of some lines of code.

--- server/server.js
+++ server/server.js
@@ -4,13 +4,13 @@ const app = express();
 const publicPath = path.join(__dirname, '..', 'public');
 const request = require('request');
 
-let url = 'http://localhost:3000/'
+let url = 'http://localhost:3000/';
 
 request(url,function(err, response, body){
     if(err){
-        console.log(`error: ${error}`)
+        //console.log(`error: ${error}`)
     } else {
-        console.log(`body: ${body}`);
+        //console.log(`body: ${body}`);
     }
 });
 
@@ -19,16 +19,16 @@ app.use(express.static(publicPath));
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(publicPath), 'index.html');
-})
+});
 
 app.get('/', (req, res) => {
-    res.render('index.html')
-})
+    res.render('index.html');
+});
 
 app.post('/postedForms', (req, res) => {
-    res.send('send back a JSON object')
-})
+    res.send('send back a JSON object');
+});
 
 app.listen(3000, () => {
-    console.log('Server is up');
-})
+    console.log('Server is up and running');
+});
