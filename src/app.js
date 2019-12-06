6cd62823d0d89e361892b584cbb0ef853cbd2dc1
Adam <adam_kirk_msc@hotmail.com>
2019-12-06 10:22:02 +0000

    Revert "Skill Setup"
    This reverts commit b655061fc4b3a3b852c7d0959747948773bdd4f9.

--- src/app.js
+++ src/app.js
@@ -1,13 +1,12 @@
 // library imports
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles/styles.sass';
+import './styles/styles.sass'
 
 // component imports
-import SkillsetDevelopment from './components/skillsetDevelopment';
-import SkillsetDesign from './components/skillsetDesign';
-import Header from './components/header';
-import Footer from './components/footer';
+import Skillset from './components/skillset'
+import Header from './components/header'
+import Footer from './components/footer'
 
 
 // This is the projects titles, change to fit needs
@@ -18,13 +17,11 @@ const footerTitle = 'I read the instructions';
 // Main stage area
 const stage = (
     <div id='stageArea'>
-        <SkillsetDevelopment skillsetName="Development"/>
-        <SkillsetDesign skillsetName="Design" />
+        <Header headerTitle={headerTitle} />
+        <Skillset />
     </div>
 )
 
 // Render elements to the html by ID
-//ReactDOM.render(stage, document.getElementById('app'));
-ReactDOM.render(<Header headerTitle={headerTitle} />, document.getElementById('header'))
-ReactDOM.render(stage, document.getElementById('app'))
+ReactDOM.render(stage, document.getElementById('app'));
 ReactDOM.render(<Footer footerTitle={footerTitle} />, document.getElementById('footer'))
---
+++ src/components/skillset.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class Skillset extends React.Component {
+    render(){
+        return (
+            <p>Hello World</p>
+        )
+    }
+}
